46b8f4b40c5c02c1f640d3778363380160bbcc24
Arthur Batista Oliveira <54534361823@info.local>
2026-04-24 15:30:52 -0300

    22/04

--- imc-javascript-main/js/script.js
+++ imc-javascript-main/js/script.js
@@ -1,67 +1,115 @@
-function calcular() {
+async function calcular() {
     const Nome = document.getElementById("nome").value;
     const Altura = parseFloat(document.getElementById("altura").value);
     const Peso = parseFloat(document.getElementById("peso").value);
 
-    console.log(Nome, Altura, Peso);
-
-    if(nome.trim().length == 0 || isNaN(altura) || isNaN(peso)){
-        alert("Preencha todos os campos");
+    if (Nome.trim().length == 0 || isNaN(Altura) || isNaN(Peso)) {
+        alert("Preencha os campos: Nome, altura e peso corretamente");
         return false;
     }
 
-function calcularImc(altura, peso) {
-    return peso / (altura * altura);
+    const valorIMC = calcularImc(Altura, Peso);
+    const resultadoTexto = gerarTextoImc(valorIMC);
 
-}
+    const objIMC = {
+        nome: Nome,
+        altura: Altura,
+        peso: Peso,
+        imc: valorIMC.toFixed(2),
+        situacao: resultadoTexto
+    };
 
-const IMC = calcularImc(Altura, Peso);
-const textoImc = gerarTextoImc(IMC);
+  
+    const sucesso = await cadastrarNaApi(objIMC);
 
-console.log(nome);
-console.log(altura);
-console.log(peso);
-console.log(IMC);
-console.log(textoImc);
+    if (sucesso) {
+       
+        buscarIMC();
 
-
-
-
-function gerarTextoImc(IMC) {
-    
-    if (IMC < 16) {
-        return "Magreza grave";
-    } else if (IMC < 17) {
-        return "Magreza moderada";
-    } else if (IMC < 18.5) {
-        return "Magreza leve";
-    } else if (IMC < 25) {
-        return "Saudável";
-    } else if (IMC < 30) {
-        return "Sobrepeso";
-    } else if (IMC < 35) {
-        return "Obesidade Grau I";
-    } else if (IMC < 40) {
-        return "Obesidade Grau II (considerada severa)";
+        
+        document.getElementById("nome").value = "";
+        document.getElementById("altura").value = "";
+        document.getElementById("peso").value = "";
+        
+        alert(`Os dados de ${Nome} foram cadastrados com sucesso!`);
     } else {
-        return "Obesidade Grau III (considerada mórbida)";
+        alert("Não foi possível cadastrar no servidor.");
     }
 }
 
-// Menor que 16 – Magreza grave;
 
-// 16 a menor que 17 – Magreza moderada;
-
-// 17 a menor que 18,5 – Magreza leve;
-
-// 18,5 a menor que 25 – Saudável;
-
-// 25 a menor que 30 – Sobrepeso;
-
-// 30 a menor que 35 – Obesidade Grau I;
-
-// 35 a menor que 40 – Obesidade Grau II (considerada severa);
-
-// Maior que 40 – Obesidade Grau III (considerada mórbida).
-
+async function cadastrarNaApi(objIMC) {
+    try {
+        let resposta = await fetch("http://localhost:3000/imc", {
+            method: "POST",
+            body: JSON.stringify(objIMC),
+            headers: {
+                "Content-Type": "application/json; charset=UTF-8"
+            }
+        });
+        return resposta.ok; 
+    } catch (error) {
+        console.error("Erro ao conectar com a API:", error);
+        return false;
+    }
 }
+
+async function buscarIMC() {
+    try {
+        const retorno = await fetch("http://localhost:3000/imc");
+        const dadosRetornos = await retorno.json();
+        const tabela = document.getElementById("cadastro");
+        
+        let template = ""; 
+
+        for (let i = 0; i < dadosRetornos.length; i++) {
+            const item = dadosRetornos[i];
+
+            template += `
+                <tr>
+                    <td>${item.nome}</td>
+                    <td>${item.altura}m</td>
+                    <td>${item.peso}kg</td>
+                    <td>${item.imc}</td>
+                    <td>${item.situacao}</td>
+                    <td>
+                        <button onclick="apagarIMC('${item.id}')" style="color: red;">Apagar</button>
+                    </td>
+                </tr>
+            `;
+        }
+
+        tabela.innerHTML = template;
+        console.log("Tabela atualizada!");
+
+    } catch (error) {
+        console.error("Erro ao buscar dados da API:", error);
+    }
+}
+
+
+async function apagarIMC(id) {
+    if (confirm("Tem certeza que deseja excluir?")) {
+        try {
+            await fetch(`http://localhost:3000/imc/${id}`, {
+                method: "DELETE"
+            });
+            buscarIMC(); 
+        } catch (error) {
+            console.log("Erro ao apagar:", error);
+        }
+    }
+}
+
+function calcularImc(altura, peso) {
+    return peso / (altura * altura);
+}
+
+function gerarTextoImc(IMC) {
+    if (IMC < 18.5) return "Magreza";
+    else if (IMC < 25) return "Saudável";
+    else if (IMC < 30) return "Sobrepeso";
+    else return "Obesidade";
+}
+
+buscarIMC();
